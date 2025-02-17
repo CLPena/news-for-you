ae52bf095f36a651fa5328a7530e2b4c031484f9
Cristina Peña <CristinaLPena@gmail.com>
2025-02-16 20:59:37 -0700

    Add scrolling to code block

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { Highlight, themes } from "prism-react-renderer"
 import { Fira_Code } from 'next/font/google'
+import { useTheme } from 'next-themes'
 
 const firaCode = Fira_Code({ subsets: ['latin'] })
 
@@ -9,28 +12,33 @@ interface CodeBlockProps {
 }
 
 export default function CodeBlock({ children, className }: CodeBlockProps) {
+    const { theme } = useTheme()
     const language = className?.replace(/language-/, '') || 'typescript'
 
     return (
-        <Highlight 
-            theme={themes.nightOwl}
-            code={children.trim()}
-            language={language}
-        >
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={`${className} ${firaCode.className} rounded-lg overflow-x-auto mb-4`} style={style}>
-                    {tokens.map((line, i) => (
-                        <div key={i} {...getLineProps({ line })} className="table-row">
-                            <span className="table-cell text-gray-500 pr-4 select-none text-right">{i + 1}</span>
-                            <span className="table-cell">
-                                {line.map((token, key) => (
-                                    <span key={key} {...getTokenProps({ token })} />
-                                ))}
-                            </span>
-                        </div>
-                    ))}
-                </pre>
-            )}
-        </Highlight>
+        <div className="max-w-3xl overflow-x-auto">
+            <Highlight
+                theme={theme === 'dark' ? themes.nightOwl : themes.github}
+                code={children}
+                language={language as any}
+            >
+                {({ tokens, getLineProps, getTokenProps }) => (
+                    <pre className={`${firaCode.className} table w-full`}>
+                        {tokens.map((line, i) => (
+                            <div key={i} {...getLineProps({ line })} className="table-row">
+                                <span className="table-cell text-right pr-4 text-gray-500 select-none w-12">
+                                    {i + 1}
+                                </span>
+                                <span className="table-cell whitespace-pre">
+                                    {line.map((token, key) => (
+                                        <span key={key} {...getTokenProps({ token })} />
+                                    ))}
+                                </span>
+                            </div>
+                        ))}
+                    </pre>
+                )}
+            </Highlight>
+        </div>
     )
 } 
